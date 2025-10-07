02bcd4450981962f16cf89fcac00d786b29f5220
Roland Szucs <rolandszucs.private@gmail.com>
2025-10-07 19:57:32 +0200

    build_unused_data_cleared

--- my-project/src/views/ExperiencesListPage.tsx
+++ my-project/src/views/ExperiencesListPage.tsx
@@ -1,14 +1,3 @@
-interface Experience {
-    id: string; // unique id
-    title: string;
-    date?: string;
-    location?: string;
-    category?: string; // Adventure or Skill
-    subcategory?: string; // Running, Skiing, Firemaking, etc.
-    photo?: string; // URL or base64
-    description?: string;
-};
-
 export default function ExperiencesListPage() {
     return (
         <>
