0fde1603e71f074cca3ffa0fcb42386852f45dcb
Roland Szucs <rolandszucs.private@gmail.com>
2025-10-10 16:52:34 +0200

    main-page-new-styles

--- my-project/src/views/HomePage.tsx
+++ my-project/src/views/HomePage.tsx
@@ -1,30 +1,36 @@
-import Searcher from '../components/HomePageComponents/Searcher';
+import Searcher from "../components/HomePageComponents/Searcher";
 
 interface HomePageProps {
-    handlePage: (page: string) => void;
-};
+  handlePage: (page: string) => void;
+}
 
-export default function HomePage({handlePage}: HomePageProps) {
 
-    
-    return (
-        <>
-        <div className='homepage-container' >
-        <Searcher
-            handlePage={handlePage}
-        />
-            
-        <button onClick={() => handlePage("addNew")} className='log-new-experience'>Log New Experience</button>
 
-        <button onClick={() => handlePage("random")} className='random-experience-button-homepage'>Generate a random experience</button>
+export default function HomePage({ handlePage }: HomePageProps) {
+  return (
+    <div className="homepage-container">
+      {/* Searcher sits at the top */}
+      <div className="searcher-component-container first-row-homepage" >
+      <Searcher handlePage={handlePage} />
+      </div>
 
-        <button onClick={() => handlePage("list")} className='logged-experiences-button-homepage' >Logged Experiences</button>
+    <div className="second-row-homepage" >
+        <button onClick={() => handlePage("addNew")} className="log-new-experience">
+            Log New Experience
+        </button>
 
-        <div className='homepage-logo-container'>
-            <img className="homepage-logo" src="/logo.png" alt="" />
-        </div>
-        
-        </div>
-        </>
-    )
+        <button onClick={() => handlePage("random")} className="random-experience-button-homepage">
+            Generate a random experience
+        </button>
+
+        <button onClick={() => handlePage("list")} className="logged-experiences-button-homepage">
+            Logged Experiences
+        </button>
+    </div>
+
+      <div className="homepage-logo-container third-row-homepage">
+        <img className="homepage-logo" src="/logo.png" alt="XtraLvl Journal logo" />
+      </div>
+    </div>
+  );
 }
